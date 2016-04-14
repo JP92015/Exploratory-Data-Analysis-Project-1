dc52a99447e3f471942a3ae22c20c116fbcf6997
JP92015 <jke5@georgetown.edu>
2016-04-14 13:46:47 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -3,9 +3,10 @@ powercons <- read.table("household_power_consumption.txt", sep = ";", header = T
 
 ## subset out Dates of interest, between "2007-02-01" and "2007-02-02"
 library(lubridate)
+library(dplyr)
 dts <- dmy_hms(paste(powercons$Date, powercons$Time))
-pcons <- cbind(dts, powercons[3:length(powercons)])
-fpcons <- filter(pcons, Date >= ymd_hms("2007-02-01 00:00:00"), Date <= ymd_hms("2007-02-02 23:59:59"))
+pcons <- cbind("datetime" = dts, powercons[3:length(powercons)])
+fpcons <- filter(pcons, datetime >= ymd_hms("2007-02-01 00:00:00"), datetime <= ymd_hms("2007-02-02 23:59:59"))
 
 ## create plot1 (histogram)
 hist(as.numeric(fpcons$Global_active_power), col = "red", main = "Global Active Power", ylim = range(0:1200),
