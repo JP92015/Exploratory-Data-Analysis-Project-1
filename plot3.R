235468c6bcdd74c8717d93a9f13bec716accd09a
JP92015 <jke5@georgetown.edu>
2016-04-14 13:47:45 -0400

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -3,15 +3,16 @@ powercons <- read.table("household_power_consumption.txt", sep = ";", header = T
 
 ## subset out Dates of interest, between "2007-02-01" and "2007-02-02"
 library(lubridate)
+library(dplyr)
 dts <- dmy_hms(paste(powercons$Date, powercons$Time))
-pcons <- cbind(dts, powercons[3:length(powercons)])
-fpcons <- filter(pcons, Date >= ymd_hms("2007-02-01 00:00:00"), Date <= ymd_hms("2007-02-02 23:59:59"))
+pcons <- cbind("datetime" = dts, powercons[3:length(powercons)])
+fpcons <- filter(pcons, datetime >= ymd_hms("2007-02-01 00:00:00"), datetime <= ymd_hms("2007-02-02 23:59:59"))
 
 ## create plot2
 with(fpcons2, plot(Date, Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub metering", cex.lab = 0.75, cex.axis = 0.75))
-lines(fpcons2$Date, fpcons2$Sub_metering_1, col = "black")
-lines(fpcons2$Date, fpcons2$Sub_metering_2, col = "red")
-lines(fpcons2$Date, fpcons2$Sub_metering_3, col = "blue")
+lines(fpcons2$datetime, fpcons2$Sub_metering_1, col = "black")
+lines(fpcons2$datetime, fpcons2$Sub_metering_2, col = "red")
+lines(fpcons2$datetime, fpcons2$Sub_metering_3, col = "blue")
 legend("topright", lty = c("solid","solid","solid"), col = c("black", "green", "blue"), 
  legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex = 0.60)
 
