decdfd03592b0db340fee1c89e9c4fa99ad71de3
JP92015 <jke5@georgetown.edu>
2016-04-14 14:11:13 -0400

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -9,10 +9,10 @@ pcons <- cbind("datetime" = dts, powercons[3:length(powercons)])
 fpcons <- filter(pcons, datetime >= ymd_hms("2007-02-01 00:00:00"), datetime <= ymd_hms("2007-02-02 23:59:59"))
 
 ## create plot2
-with(fpcons2, plot(Date, Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub metering", cex.lab = 0.75, cex.axis = 0.75))
-lines(fpcons2$datetime, fpcons2$Sub_metering_1, col = "black")
-lines(fpcons2$datetime, fpcons2$Sub_metering_2, col = "red")
-lines(fpcons2$datetime, fpcons2$Sub_metering_3, col = "blue")
+with(fpcons, plot(datetime, Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub metering", cex.lab = 0.75, cex.axis = 0.75))
+lines(fpcons$datetime, fpcons$Sub_metering_1, col = "black")
+lines(fpcons$datetime, fpcons$Sub_metering_2, col = "red")
+lines(fpcons$datetime, fpcons$Sub_metering_3, col = "blue")
 legend("topright", lty = c("solid","solid","solid"), col = c("black", "green", "blue"), 
  legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex = 0.60)
 
