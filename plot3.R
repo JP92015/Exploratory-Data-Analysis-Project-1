5b66b6b35f1cf05d7414857b167691446d883599
JP92015 <jke5@georgetown.edu>
2016-04-14 03:05:26 -0400

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -8,12 +8,12 @@ pcons <- cbind(dts, powercons[3:length(powercons)])
 fpcons <- filter(pcons, Date >= ymd_hms("2007-02-01 00:00:00"), Date <= ymd_hms("2007-02-02 23:59:59"))
 
 ## create plot2
-with(fpcons2, plot(Date, Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub metering"))
+with(fpcons2, plot(Date, Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub metering", cex.lab = 0.75, cex.axis = 0.75))
 lines(fpcons2$Date, fpcons2$Sub_metering_1, col = "black")
 lines(fpcons2$Date, fpcons2$Sub_metering_2, col = "red")
 lines(fpcons2$Date, fpcons2$Sub_metering_3, col = "blue")
 legend("topright", lty = c("solid","solid","solid"), col = c("black", "green", "blue"), 
- legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex = 0.50)
+ legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex = 0.60)
 
  ## save to png file
 dev.copy(png, file = "plot2.png")
