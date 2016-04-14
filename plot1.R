cb8d8adbaf03eeea922804079bfd46d46fc93c1d
JP92015 <jke5@georgetown.edu>
2016-04-14 00:56:08 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,9 +1,14 @@
 ## load data into R
-powercons <- read.table("household_power_consumption.txt", sep = ";")
+powercons <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, na.string = "?")
 
 ## subset out Dates of interest, between "2007-02-01" and "2007-02-02"
 pcons <- mutate(powercons, Date = as.Date(powercons$Date, format = "%d/%m/%Y"))
 fpcons <- filter(pcons, Date >= as.Date("2007-02-01"), Date <= as.Date("2007-02-02"))
 
 ## create plot1 (histogram)
-hist(as.numeric(fpcons$Global_active_power), col = "red", main = "Global Active Power")
+hist(as.numeric(fpcons$Global_active_power), col = "red", main = "Global Active Power", ylim = range(0:1200),
+ xlab = "Global Active Power (kilowatts)")
+ 
+ ## save to png file
+dev.copy(png, file = "plot1.png")
+dev.off()
